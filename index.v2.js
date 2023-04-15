ba7dcb3d5c32b998fdf9e2251a09ed331967592c
Papai Sarkar <iampapaisarkar@gmail.com>
2023-04-16 02:51:32 +0530

    ~ Enhancement: Add vue2 and vue3 version

---
+++ index.v2.js
@@ -0,0 +1,37 @@
+function detectPlatform() {
+    const userAgent = navigator.userAgent;
+  
+    if (/mobile/i.test(userAgent)) {
+      return "mobile";
+    } else if (/tablet/i.test(userAgent)) {
+      return "tablet";
+    } else {
+      return "web";
+    }
+  }
+  
+  export default {
+    install(Vue) {
+      let currentPlatform = detectPlatform();
+      Vue.prototype.$platform = currentPlatform;
+  
+      window.addEventListener("resize", () => {
+        const newPlatform = detectPlatform();
+  
+        if (newPlatform !== currentPlatform) {
+          currentPlatform = newPlatform;
+          Vue.prototype.$platform = currentPlatform;
+        }
+      });
+  
+      window.addEventListener("orientationchange", () => {
+        const newPlatform = detectPlatform();
+  
+        if (newPlatform !== currentPlatform) {
+          currentPlatform = newPlatform;
+          Vue.prototype.$platform = currentPlatform;
+        }
+      });
+    }
+  };
+  
